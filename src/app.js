dc0c3b599402c2b0d3b02d9c9125d26280301615
Maximo Garcia <maximogarcia006@gmail.com>
2022-11-14 13:45:48 -0300

    solucion

--- src/app.js
+++ src/app.js
@@ -25,16 +25,16 @@ app.use('', require('./routes/users'))
 app.get('/api/user', verifyToken, controllers.getUserByID)
  
 
-app.post('/api/user/cursos/:_id', cursosCtrl.createCursos)
-app.get('/api/user/:_id/cursos', cursosCtrl.getCursos)
-app.delete('/api/user/:_id1/curso/:_id2/delete', cursosCtrl.deleteCurso)
-app.get('/api/user/:_id1/curso/:_id2', cursosCtrl.getCurso)
+app.post('/api/user/cursos/:_id',cors(), cursosCtrl.createCursos)
+app.get('/api/user/:_id/cursos', cors(),cursosCtrl.getCursos)
+app.delete('/api/user/:_id1/curso/:_id2/delete',cors(), cursosCtrl.deleteCurso)
+app.get('/api/user/:_id1/curso/:_id2', cors(),cursosCtrl.getCurso)
 
-app.get('/api/students/get', StudentCtrl.getStudentsGral)
-app.post('/api/user/:_id1/curso/students/:_id2', StudentCtrl.createStudent)
-app.get('/api/user/curso/:_id/students', StudentCtrl.getStudents) 
-app.get('/api/user/curso/student/:_id', StudentCtrl.getStudent)
-app.put('/api/student/:_id',StudentCtrl.updateStudent)
-app.delete('/api/user/student/:_id1/curso/:_id2/delete', StudentCtrl.deleteStudent)
+app.get('/api/students/get', cors(),StudentCtrl.getStudentsGral)
+app.post('/api/user/:_id1/curso/students/:_id2', cors(),StudentCtrl.createStudent)
+app.get('/api/user/curso/:_id/students', cors(),StudentCtrl.getStudents) 
+app.get('/api/user/curso/student/:_id', cors(),StudentCtrl.getStudent)
+app.put('/api/student/:_id',cors(),StudentCtrl.updateStudent)
+app.delete('/api/user/student/:_id1/curso/:_id2/delete', cors(),StudentCtrl.deleteStudent)
 
 module.exports = app;
