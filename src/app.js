1fe1651246933e3c951cf3aff27dbc85f7799d66
Maximo Garcia <maximogarcia006@gmail.com>
2022-11-14 14:19:04 -0300

    a

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require('cors')
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://listado-de-alumnos-maximogarcia.vercel.app");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
