c528e9671ebd131c6456d180289ead9e6e0375e3
Maximo Garcia <maximogarcia006@gmail.com>
2022-11-14 14:06:10 -0300

    a

--- src/app.js
+++ src/app.js
@@ -17,14 +17,15 @@ app.use(cors({
 }))
 app.use(express.json()) 
 
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "https://listado-de-alumnos-maximogarcia.vercel.app");
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept"
     );
     next();
   });
+  
 
 app.set('port', process.env.PORT || 4000) 
 app.get('/', (req,res)=>res.send('h'))
